3f8ef0260f76aa4ae678a52cb37a421f478a08ff
imrany <imranmat254@gmail.com>
2023-06-05 22:51:48 +0300

    chat window ui issues not fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,5 @@
 // @flow strict
 function Home() {
-    const showInput=()=>{
-        let keyboard=document.getElementById("keyboard") as HTMLDivElement
-        keyboard.style.display="none"
-        let input=document.getElementById("show-input") as HTMLDivElement
-        input.style.display="block"
-    }
-    const handleSubmit=(e:any)=>{
-        e.preventDefault()
-        const request:string=e.target.request.value
-        console.log(request)
-    }
 
     const data=[
         {
@@ -30,12 +19,30 @@ function Home() {
             response:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea odio aperiam reprehenderit tempora? Eligendi rerum reprehenderit, omnis quasi adipisci assumenda? Quae, aperiam deserunt. Alias architecto minima ratione? Facere, nesciunt dolore?"
         }
     ]
+
+    const showInput=()=>{
+        let keyboard=document.getElementById("keyboard") as HTMLDivElement
+        keyboard.style.display="none"
+        let input=document.getElementById("show-input") as HTMLDivElement
+        input.style.display="block"
+    }
+    const handleSubmit=(e:any)=>{
+        e.preventDefault()
+        const request:string=e.target.request.value
+        const output={
+            request:request,
+            response:"Hello"
+        }
+        data.push(output)
+        console.log(data)
+        e.target.reset()
+    }
     return (
-        <div>
-            <div className="px-6 py-8 my-14" id="window">
+        <div className="md:flex md:justify-center">
+            <div className="px-6 py-8 my-14 md:w-[80vw] " id="window">
                 {data&&data.map((i,n)=>(
                     <div key={n} className="mb-20">
-                        <div className="bg-gray-300 px-4 py-2 rounded-[20px] w-fit h-fit absolute right-0 -mt-12">
+                        <div className="bg-gray-300 px-4 py-2 rounded-[20px] w-fit h-fit float-right -mt-12">
                             <p className='text-base max-sm:text-sm text-center'>{i.request}</p>
                         </div>
 
@@ -49,7 +56,7 @@ function Home() {
             <div className="fixed bottom-16 right-14 max-sm:right-8 cursor-pointer" id="keyboard" onClick={showInput}>
                 <i className="ri-keyboard-fill ri-2x text-gray-700"></i>
             </div>
-            <div id="show-input" className="fixed bottom-14 left-0 right-0 border-[1px]">
+            <div id="show-input" className="fixed bottom-14 left-0 right-0 border-[1px] bg-white">
                 <form className="flex justify-between" onSubmit={handleSubmit}>
                     <input placeholder="Type a message" name="request" className="w-full px-4 pb-4 pt-3 text-base "/>
                     <button><i className="ri-message-fill ri-lg px-3 text-gray-600"></i></button>
