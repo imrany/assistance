77bd5488c903ac6b7b8121075062469809f0bcd0
imrany <imranmat254@gmail.com>
2023-06-06 20:26:10 +0300

    status notice and redirect done --works

--- src/pages/Fallback.tsx
+++ src/pages/Fallback.tsx
@@ -1,7 +1,9 @@
+import { panel } from "../components/panel";
+
 // @flow strict
 function Fallback() {
     return (
-        <div className='flex flex-col h-[75vh] justify-center items-center'>
+        <div className='flex flex-col h-[75vh] justify-center items-center' onClick={panel.close}>
            <div className="border-[1px] px-3 py-3 rounded-[20px] max-md:w-[90vw]">
                 <p className='text-base max-sm:text-sm text-center'>Oops! Currently you cannot view this page.</p>
            </div>
