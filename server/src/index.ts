cc823adab3ce15a77a43a4056ef1bd9177700c0a
imrany <imranmat254@gmail.com>
2023-06-07 17:50:47 +0300

    decided to go with natural not brain.js

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors({}))
 
-const port=process.env.PORT||8000
+const port=process.env.PORT||5000
 app.listen(port,()=>{
     console.log(`server running port ${port}`)
 })
