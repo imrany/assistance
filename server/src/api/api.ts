98b7b36af6ff1faa4e71e06987b90efd03951c98
imrany <imranmat254@gmail.com>
2023-06-11 20:38:21 +0300

    added index counter

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -2,13 +2,13 @@ import express from "express"
 import { modal } from "../natural"
 const router=express.Router()
 
-
+let index=0;
 router.post("/chat",async(req:any,res:any)=>{
-    
+    ++index
     try {
         const {request}=req.body
         const response=modal(request)
-        res.status(200).send({response:response})
+        res.status(200).send({response:response,index:index})
     } catch (error:any) {
         res.status(500).send({error:error.message})
     }
