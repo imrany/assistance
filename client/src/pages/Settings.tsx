2ffac10b757241ecca018dc3d1af60f572cf99b1
imrany <imranmat254@gmail.com>
2023-06-12 13:55:53 +0300

    added app icons and updated the manifest.json

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -9,6 +9,9 @@ function Settings() {
     return (
         <div className="flex flex-col py-4 px-4">
             <PageHeader val={val}/>
+            <div className="lg:px-14 py-6 ">
+                <p className="text-lg text-blue-500">Account</p>
+            </div>
         </div>
     );
 };
