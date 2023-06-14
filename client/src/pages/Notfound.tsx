ce6721fe8c2c6c6a21955911ea87bd3c4eb4909a
imrany <imranmat254@gmail.com>
2023-06-14 19:09:52 +0300

    added Fallback route

--- client/src/pages/Notfound.tsx
+++ client/src/pages/Notfound.tsx
@@ -1,10 +1,8 @@
-import { panel } from "../components/func";
-
 // @flow strict
 function Notfound() {
     return (
         <>
-            <div className='flex flex-col h-[100vh] justify-center items-center' onClick={panel.close}>
+            <div className='flex flex-col h-[100vh] justify-center items-center'>
                 <p className="text-5xl mb-2 text-gray-700 font-semibold text-center">404</p>
                 <p className='text-lg text-gray-600 max-sm:text-sm text-center'>This page does not exit.</p>
             </div>
