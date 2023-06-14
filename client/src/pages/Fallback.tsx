ad5f5ea890e4b19c751e85f7e743dd7b59902ff8
imrany <imranmat254@gmail.com>
2023-06-14 19:14:12 +0300

    added Fallback route

--- client/src/pages/Fallback.tsx
+++ client/src/pages/Fallback.tsx
@@ -3,7 +3,6 @@ function Fallback() {
     return (
         <>
              <div className='flex flex-col h-[100vh] justify-center items-center'>
-                <p className="text-5xl font-semibold text-center"></p>
                 <i className="ri-cloud-off-line ri-3x text-gray-700"></i>
                 <p className='text-lg text-gray-600 max-sm:text-sm text-center'>You can view this page offline.</p>
             </div>
--- client/src/pages/Notfound.tsx
+++ client/src/pages/Notfound.tsx
@@ -3,7 +3,7 @@ function Notfound() {
     return (
         <>
             <div className='flex flex-col h-[100vh] justify-center items-center'>
-                <p className="text-5xl mb-2 text-gray-700 font-semibold text-center">404</p>
+                <p className="text-5xl max-sm:text-3xl mb-2 text-gray-700 font-semibold text-center">404</p>
                 <p className='text-lg text-gray-600 max-sm:text-sm text-center'>This page does not exit.</p>
             </div>
         </>
