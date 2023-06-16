4991677efccbd142af5accd7ff35e108c6671b78
imrany <imranmat254@gmail.com>
2023-06-16 15:30:54 +0300

    code cleaning --removing dead code

--- server/archive/index.ts
+++ server/archive/index.ts
@@ -1,41 +1,18 @@
 import csv from "csvtojson"
-import { createReadStream, readFile, writeFile } from "fs"
-import { createInterface } from "readline"
+import { readFile, writeFile } from "fs"
 const csv_file_path=`${__dirname}/S10_question_answer_pairs.csv`
- 
-let output_file=`${__dirname}/outexample.csv`
+let output_file=`${__dirname}/final.csv`
+
 // csv()
 //     .fromFile(csv_file_path)
 //     .then((jsonObj)=>{
 //         console.log(jsonObj)
 //     })
 
-async function processLineByLine() {
-  const fileStream =createReadStream(csv_file_path);
-  const rl = createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-  for await (const line of rl) {
-    let new_data=line.split("    ").join(",")
-    writeFile(`${__dirname}/outexample.csv`,new_data,(data:any)=>{
-        console.log('File added')
-    })
-    // csv()
-    // .fromFile(csv_file_path)
-    // .then((jsonObj)=>{
-    //     console.log(jsonObj)
-    // })
-    // console.log(new_data);
-    // Each line in input.txt will be successively available here as `line`.
-    // console.log(`Line from file: ${line}`);
-  }
-}
-
 const change_tocsv_format=()=>{
     readFile(output_file,"utf8",(err:any,data:any)=>{
         let new_data=data.split("    ").join(",")
-        writeFile(`${__dirname}/outexamplescvformat.csv`,new_data,()=>{
+        writeFile(output_file,new_data,()=>{
             console.log('File added')
         })
     })
