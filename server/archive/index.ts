30ed817008c5b8705a6785118b655bb57e58c00f
imrany <imranmat254@gmail.com>
2023-06-16 14:14:02 +0300

    using replace

--- server/archive/index.ts
+++ server/archive/index.ts
@@ -1,8 +1,14 @@
 import csv from "csvtojson"
 const csv_file_path=`${__dirname}/example.csv`
+ 
+let str="hello string and people"
+// csv()
+//     .fromFile(csv_file_path)
+//     .then((jsonObj)=>{
+//         console.log(jsonObj)
+//     })
 
-csv()
-    .fromFile(csv_file_path)
-    .then((jsonObj)=>{
-        console.log(jsonObj)
-    })
+const replace=()=>{
+   let new_str= str.replace(/ /g,",")
+   console.log(new_str)
+}
