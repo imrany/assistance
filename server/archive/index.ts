33aa795fd030c42bb6e3eb2bb8c40a5cbda31437
imrany <imranmat254@gmail.com>
2023-06-16 14:32:28 +0300

    readfile

--- server/archive/index.ts
+++ server/archive/index.ts
@@ -1,4 +1,5 @@
 import csv from "csvtojson"
+import { readFile } from "fs"
 const csv_file_path=`${__dirname}/example.csv`
  
 let str="hello  string  and people"
@@ -8,9 +9,15 @@ let str="hello  string  and people"
 //         console.log(jsonObj)
 //     })
 
+const read_file=()=>{
+    readFile(csv_file_path,"utf8",(err:any,data:any)=>{
+        // Display the file content
+        console.log(data);
+    })
+}
 
 const split=(str:any)=>{
     let new_str= str.split("  ").join("+")
    console.log(new_str)
 }
-split(csv_file_path)
+read_file()
