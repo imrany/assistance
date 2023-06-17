d57a907889d2a3d7b09b29130f4a710d93714337
imrany <imranmat254@gmail.com>
2023-06-17 13:18:08 +0300

    resolved the get started route problem

--- client/src/components/UI/Footer.tsx
+++ client/src/components/UI/Footer.tsx
@@ -4,11 +4,11 @@ import { useNavigate } from "react-router-dom";
 function Footer() {
     const navigate=useNavigate()
     const close=()=>{
+        navigate("/auth/sign_in")
         const footer=document.querySelector("footer") as HTMLElement
         const input=document.getElementById("show-input") as HTMLDivElement
         footer.classList.add("close")
         input.style.bottom="0"
-        navigate("/auth/sign_in")
     }
     return (
         <footer className="fixed bottom-0 left-0 right-0 text-white bg-blue-600 flex justify-between py-5 px-20 max-md:px-4">
