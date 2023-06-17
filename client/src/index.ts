bacb9c40ee0bb43b230764f5567b484af807dad5
imrany <imranmat254@gmail.com>
2023-06-17 11:28:11 +0300

    Revert "errors"
    This reverts commit 0b964b0414cee1d3002aab5de11094b95c16a686.

--- client/src/index.ts
+++ client/src/index.ts
@@ -20,9 +20,3 @@ if(Notification.permission === 'granted'){
          }
      });
  };
-
-if('serviceWorker' in navigator){
-   navigator.serviceWorker.register("./sw.js")
-   .then(()=>console.log('sw registered'))
-   .catch((err)=>console.log('sw not registerd', err))
- }
