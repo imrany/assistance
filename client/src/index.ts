0b964b0414cee1d3002aab5de11094b95c16a686
imrany <imranmat254@gmail.com>
2023-06-17 11:27:51 +0300

    errors

--- client/src/index.ts
+++ client/src/index.ts
@@ -20,3 +20,9 @@ if(Notification.permission === 'granted'){
          }
      });
  };
+
+if('serviceWorker' in navigator){
+   navigator.serviceWorker.register("./sw.js")
+   .then(()=>console.log('sw registered'))
+   .catch((err)=>console.log('sw not registerd', err))
+ }
