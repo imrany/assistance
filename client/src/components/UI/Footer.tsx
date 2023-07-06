5b36437afd3cb19519dbcf20a805f537aa9d50d9
imrany <imranmat254@gmail.com>
2023-07-06 23:53:47 +0300

    footer update

--- client/src/components/UI/Footer.tsx
+++ client/src/components/UI/Footer.tsx
@@ -2,13 +2,19 @@ import logo from "/favicon.png"
 // @flow strict
 function Footer() {
     return (
-        <footer className="fixed bottom-0 left-0 right-0 bg-gray-50 rounded-t-[15px] shadow-2xl flex justify-between py-5 px-20 max-md:px-4" id="install_indicator"  hidden>
-            <div className="flex">
-                <img src={logo} alt="." className="w-8 h-8 rounded-[5px]"/>
-                <p className="ml-2 max-md:text-sm my-1 font-semibold text-base">Assistance</p>
-            </div>
-            <div className="bg-blue-600 py-2 text-white px-5 rounded-[30px] font-semibold max-md:text-sm cursor-pointer" id="install" title="Install Assistance">Install</div>
-        </footer>
+        <>
+        {!localStorage.getItem("installed")?(
+            <footer className="fixed bottom-0 left-0 right-0 bg-gray-50 rounded-t-[15px] shadow-2xl flex justify-between py-5 px-20 max-md:px-4" id="install_indicator"  hidden>
+                <div className="flex">
+                    <img src={logo} alt="." className="w-8 h-8 rounded-[5px]"/>
+                    <p className="ml-2 max-md:text-sm my-1 font-semibold text-base">Assistance</p>
+                </div>
+                <div className="bg-blue-600 py-2 text-white px-5 rounded-[30px] font-semibold max-md:text-sm cursor-pointer" id="install" title="Install Assistance">Install</div>
+            </footer>
+        ):(
+            <></>
+        )}
+        </>
     );
 };
 
--- client/src/components/UI/Window.tsx
+++ client/src/components/UI/Window.tsx
@@ -13,7 +13,7 @@ function Window(prop:Props) {
                     </div>
 
                     <div className="border-[1px] px-4 py-2 rounded-[20px] w-fit h-fit">
-                        {!i.response.includes("Hello")?
+                        {i.response!=="NULL"?
                             <p className='text-base max-sm:text-sm text-center' title="AI's response">{i.response}</p>
                         :<p className='text-base max-sm:text-sm  text-red-600'>Cannot generate a valid response. Find out from other sources...</p>}
                         <div className="mt-4 mb-2 py-1 px-2 rounded-[30px] bg-gray-200 text-gray-600 text-sm  w-fit text-center  hover:bg-gray-800 hover:text-stone-100">
